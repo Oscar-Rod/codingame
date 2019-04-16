6dfc793c7c41a5a9a91bd3d43f43fa4b3096e3d8
Oscar Rodriguez <rguez.r.os@gmail.com>
2019-04-16 14:19:22 +0000

    deleted old implementation

--- codinegame/src/main/java/rodriguez/codingame/Player.java
+++ codinegame/src/main/java/rodriguez/codingame/Player.java
@@ -1,6 +1,8 @@
 package rodriguez.codingame;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 /**
  * Auto-generated code below aims at helping you parse
@@ -82,8 +84,16 @@ class GameEngine {
     TroopsList troops;
     BombsList bombs;
     List<Integer> enemyFactoriesAttacked = new ArrayList<>();
+    List<String> actions = new ArrayList<>();
+    List[] myFactoriesInDanger = new ArrayList[4];
 
-    GameEngine(){};
+
+    GameEngine() {
+        myFactoriesInDanger[0] = new ArrayList<Integer>();
+        myFactoriesInDanger[1] = new ArrayList<Integer>();
+        myFactoriesInDanger[2] = new ArrayList<Integer>();
+        myFactoriesInDanger[3] = new ArrayList<Integer>();
+    }
 
     public void setMap(FactoriesMap map) {
         this.map = map;
@@ -101,126 +111,157 @@ class GameEngine {
         this.bombs = bombs;
     }
 
-    public void move(){
-        // System.out.println("WAIT");
-        List myFactoriesInDanger = getMyFactoriesInDanger();
+    public void move() {
         enemyFactoriesAttacked.clear();
+        actions.clear();
+        myFactoriesInDanger[0].clear();
+        myFactoriesInDanger[1].clear();
+        myFactoriesInDanger[2].clear();
+        myFactoriesInDanger[3].clear();
+        getMyFactoriesInDanger();
 
-        String completeMove = "";
-        for (Factory myFactory : factories.getMyFactories()) {
-            if (myFactoriesInDanger.contains(myFactory.getId())) {
-                continue;
-            }
-            completeMove = upgradeMyFactory(myFactory, completeMove);
-            completeMove = attackEnemyFactories(myFactory, completeMove);
-            completeMove = attackNeutralFactories(myFactory, completeMove);
-            //if (!bombSent) completeMove = sendBomb(myFactory, completeMove);
-        }
-
-        if (!completeMove.isEmpty()) System.out.println(completeMove);
-        else System.out.println("WAIT");
-    }
-
-    private String upgradeMyFactory(Factory myFactory, String completeMove) {
-        if (myFactory.getCyborgs() - getEnemyReinforcements(myFactory) > 10) {
-            completeMove = addAction(completeMove, "INC " + myFactory.getId());
-            myFactory.setCyborgs(myFactory.getCyborgs() - 10);
-        }
-        return completeMove;
-    }
-
-    private List getMyFactoriesInDanger() {
-        List<Integer> myFactoriesInDanger = new ArrayList<>();
         for (Factory myFactory : factories.getMyFactories()){
-            if (myFactory.getCyborgs() - getEnemyReinforcements(myFactory) < 0) {
-                myFactoriesInDanger.add(myFactory.getId());
-            }
+            //TODO: Need a system to priorityze attacking nearer factories
+            setNumberOfCyborgsToTheMaximumItIsSafeToSpend(myFactory);
+            upgradeMyFactory(myFactory);
+            attackEnemyFactories(myFactory, 3);
+            attackEnemyFactories(myFactory, 2);
+            attackNeutralFactories(myFactory, 3);
+            attackNeutralFactories(myFactory, 2);
+            attackEnemyFactories(myFactory, 1);
+            attackNeutralFactories(myFactory, 1);
         }
-        return myFactoriesInDanger;
+        executeActions();
     }
 
-    private String attackNeutralFactories(Factory myFactory, String completeMove) {
+    private void setNumberOfCyborgsToTheMaximumItIsSafeToSpend(Factory myFactory) {
+        int incomingEnemyTroops = enemyTroopsGoingToFactoryPonderingDistance(myFactory);
+        if (incomingEnemyTroops < 0) return;
+        if (incomingEnemyTroops > myFactory.getCyborgs()) myFactory.setCyborgs(0);
+        else myFactory.setCyborgs(myFactory.getCyborgs() - incomingEnemyTroops);
+    }
+
+    private void getMyFactoriesInDanger() {
+        for (Factory myFactory : factories.getMyFactories()){
+            if (myFactory.getCyborgs() < enemyTroopsGoingToFactoryPonderingDistance(myFactory)) {
+                System.err.println("Factory " + myFactory.getId() + " in danger");
+                myFactoriesInDanger[myFactory.getProduction()].add(myFactory.getId());
+            }
+        }
+    }
+
+    private int enemyTroopsGoingToFactoryPonderingDistance(Factory target) {
+        //TODO: now if 2 troops attack at same time, the danger is ignored. If prod is 3, and next turn 2 troops of 3 each arrive, both are individually ignored, as 3 - 1 * 3 = 0
+        //TODO: if 1 troop of 6 attacks, the danger is acknowledged. If prod is 3: 6 - 1 * 3 = 3
+        int numberOfTroops = 0;
+        for (Troop troop : troops.getEnemyTroops()) {
+            if (troop.getTarget() == target.getId()) {
+                //System.err.println("troops: " + troop.getCyborgs() + " TTO: " + troop.getTimeToObjective() + " target: " + target.getId());
+                int incomingEnemyTroops = (troop.getCyborgs() - troop.getTimeToObjective() * target.getProduction()) < 0 ? 0 : (troop.getCyborgs() - troop.getTimeToObjective() * target.getProduction());
+                //System.err.println("troops calculated: " + incomingEnemyTroops);
+                numberOfTroops = numberOfTroops + incomingEnemyTroops;
+                //System.err.println("Total troops: " + numberOfTroops);
+            }
+        }
+        //System.err.println("Total troops after loop: " + numberOfTroops);
+        return numberOfTroops;
+    }
+
+    private void upgradeMyFactory(Factory myFactory) {
+        if (myFactory.getProduction() < 3 && myFactory.getCyborgs() > 10) {
+            actionUpgrade(myFactory);
+        }
+    }
+
+    private void attackNeutralFactories(Factory myFactory, int level) {
         for (Factory neutralFactory : factories.getNeutralFactories()) {
-            for (int i = 3; i > 0; i--) {
-                if (neutralFactory.getProduction() == i) {
-                    int enemyTroops = neutralFactory.getCyborgs();
-                    int neededTroops = enemyTroops - calculateReinforcements(neutralFactory) + 1;
-                    int availableTroops = getAvailableTroops(myFactory);
-                    if (availableTroops > neededTroops && neededTroops > 0) {
-                        myFactory.setCyborgs(myFactory.getCyborgs() - neededTroops);
-                        completeMove = addAction(completeMove, "MOVE " + myFactory.getId() + " " + neutralFactory.getId() + " " + neededTroops);
-                    }
-                }
-            }
+            if (neutralFactory.getProduction() == level) attackFactory(myFactory, neutralFactory);
         }
-        return completeMove;
     }
 
-    private String attackEnemyFactories(Factory myFactory, String completeMove) {
+    private void attackEnemyFactories(Factory myFactory, int level){
         for (Factory enemyFactory : factories.getEnemyFactories()) {
-            if (enemyFactoriesAttacked.contains(enemyFactory.getId())) continue;
-            int distance = map.getDistance(myFactory.getId(), enemyFactory.getId());
-            for (int i = 3; i > 0; i--) {
-                if (enemyFactory.getProduction() == i) {
-                    int neededTroops = getNeededTroops(enemyFactory, distance);
-                    int availableTroops = getAvailableTroops(myFactory);
-                    if (availableTroops > neededTroops && neededTroops > 0) {
-                        myFactory.setCyborgs(myFactory.getCyborgs() - neededTroops);
-                        completeMove = addAction(completeMove, "MOVE " + myFactory.getId() + " " + enemyFactory.getId() + " " + neededTroops);
-                        enemyFactoriesAttacked.add(enemyFactory.getId());
-                    }
-                }
+            if (enemyFactory.getProduction() == level) attackFactory(myFactory, enemyFactory);
+        }
+    }
+
+    private void attackFactory(Factory myFactory, Factory target){
+        //TODO: Need to ponder my troops going, to avoid a stream of troops of 1 unit. If I need 10 troops to conquer, and I have 11 troops of 1 unit going, I am not sending more. But if production is 1, I actually need more
+        int myTroopsInTheFactory = myFactory.getCyborgs();
+        int enemyTroopsInTheFactory = target.getCyborgs();
+        int myTroopsGoingToTheFactory = myTroopsGoingToFactory(target);
+        int enemyTroopsGoingToTheFactory = enemyTroopsGoingToFactory(target);
+        int enemyTroopsProducedInTheFactory = target.getOwner() == 0 ? 1 : target.getProduction() * (map.getDistance(myFactory.getId(), target.getId()) + 1);
+
+        boolean isNotAlreadyBeingConquered = !(myTroopsGoingToTheFactory - enemyTroopsInTheFactory - enemyTroopsGoingToTheFactory - enemyTroopsProducedInTheFactory > 0);
+        boolean canIConquerIt = myTroopsInTheFactory + myTroopsGoingToTheFactory - enemyTroopsInTheFactory - enemyTroopsGoingToTheFactory - enemyTroopsProducedInTheFactory > 0;
+
+        int neededTroopsForConquer = enemyTroopsInTheFactory + enemyTroopsGoingToTheFactory + enemyTroopsProducedInTheFactory - myTroopsGoingToTheFactory;
+
+        if (isNotAlreadyBeingConquered && canIConquerIt && neededTroopsForConquer > 0 && !enemyFactoriesAttacked.contains(target.getId())) {
+            actionMove(myFactory, target, neededTroopsForConquer);
+            enemyFactoriesAttacked.add(target.getId());
+        }
+
+//        System.err.println("FACTORY ORIGIN: " + myFactory.getId() + "------- FACTORY TARGET: " + target.getId());
+//        System.err.println("myTroopsInTheFactory " + myTroopsInTheFactory);
+//        System.err.println("enemyTroopsInTheFactory " + enemyTroopsInTheFactory);
+//        System.err.println("myTroopsGoingToTheFactory " + myTroopsGoingToTheFactory);
+//        System.err.println("enemyTroopsGoingToTheFactory " + enemyTroopsGoingToTheFactory);
+//        System.err.println("enemyTroopsProducedInTheFactory " + enemyTroopsProducedInTheFactory);
+//        System.err.println("isNotAlreadyBeingConquered " + isNotAlreadyBeingConquered);
+//        System.err.println("canIConquerIt " + canIConquerIt);
+//        System.err.println("neededTroopsForConquer " + neededTroopsForConquer);
+    }
+
+    private int enemyTroopsGoingToFactory(Factory target) {
+        int numberOfTroops = 0;
+        for (Troop troop : troops.getEnemyTroops()) {
+            if (troop.getTarget() == target.getId()) {
+                numberOfTroops += troop.getCyborgs();
             }
         }
-        return completeMove;
+        return numberOfTroops;
     }
 
-    private int getAvailableTroops(Factory myFactory) {
-        return myFactory.getCyborgs() - getEnemyReinforcements(myFactory);
-    }
-
-    private int getNeededTroops(Factory enemyFactory, int distance) {
-        int currentEnemyTroops = enemyFactory.getCyborgs();
-        int production = enemyFactory.getProduction();
-        int predictedEnemyTroops = currentEnemyTroops + 1 + distance * production;
-        return predictedEnemyTroops - calculateReinforcements(enemyFactory) + 1;
-    }
-
-//    private String sendBomb(Factory myFactory, String completeMove) {
-//        if (enemyFactory.getCyborgs() >= 10 && enemyFactory.getProduction() > 1 && distance <10) {
-//                completeMove = addAction(completeMove, "BOMB " + myFactory.getId() + " " + enemyFactory.getId());
-//        }
-//    }
-
-    private int calculateReinforcements(Factory target){
-        int reinforcements = 0;
-        reinforcements -= getEnemyReinforcements(target);
-        reinforcements += getMyReinforcements(target);
-        return reinforcements;
-    }
-
-    private int getMyReinforcements(Factory target) {
-        int reinforcements = 0;
-        for (Troop troop : troops.getMyTroops()){
-            if (troop.getTarget() == target.getId()) reinforcements += troop.getCyborgs();
+    private int myTroopsGoingToFactory(Factory target) {
+        int numberOfTroops = 0;
+        for (Troop troop : troops.getMyTroops()) {
+            if (troop.getTarget() == target.getId()) {
+                numberOfTroops += troop.getCyborgs();
+            }
         }
-        return reinforcements;
+        return numberOfTroops;
     }
 
-    private int getEnemyReinforcements(Factory target) {
-        int reinforcements = 0;
-        for (Troop troop : troops.getEnemyTroops()){
-            if (troop.getTarget() == target.getId()) reinforcements += troop.getCyborgs();
-        }
-        return reinforcements;
+    private void actionUpgrade(Factory factory){
+        addAction("INC " + factory.getId());
+        factory.setCyborgs(factory.getCyborgs() - 10);
     }
 
-    private String addAction(String completeTurn, String nextAction) {
-        if (completeTurn.isEmpty()) completeTurn = nextAction;
+    private void actionBomb(Factory origin, Factory target){
+        addAction("BOMB " + origin.getId() + " " + target.getId());
+    }
+
+    private void actionMove(Factory origin, Factory target, int numberOfTroops){
+        addAction("MOVE " + origin.getId() + " " + target.getId() + " " + numberOfTroops);
+        origin.setCyborgs(origin.getCyborgs() - numberOfTroops);
+    }
+
+    private void addAction(String action){
+       actions.add(action);
+    }
+
+    private void executeActions(){
+        if (actions.isEmpty()) System.out.println("WAIT");
         else {
-            completeTurn = completeTurn + ";" + nextAction;
+            StringBuilder nextMove = new StringBuilder(actions.get(0));
+            for (int i = 1; i < actions.size(); i++){
+                nextMove.append(";").append(actions.get(i));
+            }
+            System.out.println(nextMove.toString());
         }
-        return completeTurn;
+
     }
 }
 
@@ -228,18 +269,19 @@ class GameEngine {
 class FactoriesMap {
     private int[][] factories;
 
-    public FactoriesMap(){};
+    public FactoriesMap() {
+    }
 
-    public void setMapSize(int numberOfFactories){
+    public void setMapSize(int numberOfFactories) {
         factories = new int[numberOfFactories][numberOfFactories];
     }
 
-    public void add(int factory1, int factory2, int distance){
+    public void add(int factory1, int factory2, int distance) {
         factories[factory1][factory2] = distance;
         factories[factory2][factory1] = distance;
     }
 
-    public int getDistance(int factory1, int factory2){
+    public int getDistance(int factory1, int factory2) {
         return factories[factory1][factory2];
     }
 }
@@ -249,33 +291,34 @@ class FactoriesList {
     List enemyFactories = new ArrayList<Factory>();
     List neutralFactories = new ArrayList<Factory>();
 
-    public FactoriesList(){}
+    public FactoriesList() {
+    }
 
-    public void setFactory(Factory factory){
+    public void setFactory(Factory factory) {
         if (factory.getOwner() == 1) myFactories.add(factory);
         else if (factory.getOwner() == -1) enemyFactories.add(factory);
         else neutralFactories.add(factory);
     }
 
-    public void resetList(){
+    public void resetList() {
         myFactories.clear();
         neutralFactories.clear();
         enemyFactories.clear();
     }
 
-    public List<Factory> getMyFactories(){
+    public List<Factory> getMyFactories() {
         return myFactories;
     }
 
-    public List<Factory> getEnemyFactories(){
+    public List<Factory> getEnemyFactories() {
         return enemyFactories;
     }
 
-    public void setEnemyFactories(List<Factory> newEnemyFactories){
+    public void setEnemyFactories(List<Factory> newEnemyFactories) {
         enemyFactories = newEnemyFactories;
     }
 
-    public List<Factory> getNeutralFactories(){
+    public List<Factory> getNeutralFactories() {
         return neutralFactories;
     }
 
@@ -285,23 +328,24 @@ class TroopsList {
     List enemyTroops = new ArrayList<Troop>();
     List myTroops = new ArrayList<Troop>();
 
-    public TroopsList(){}
+    public TroopsList() {
+    }
 
-    public void resetList(){
+    public void resetList() {
         enemyTroops.clear();
         myTroops.clear();
     }
 
-    public void setTroop(Troop troop){
+    public void setTroop(Troop troop) {
         if (troop.getOwner() == 1) myTroops.add(troop);
         else enemyTroops.add(troop);
     }
 
-    public List<Troop> getEnemyTroops(){
+    public List<Troop> getEnemyTroops() {
         return enemyTroops;
     }
 
-    public List<Troop> getMyTroops(){
+    public List<Troop> getMyTroops() {
         return myTroops;
     }
 }
@@ -310,23 +354,24 @@ class BombsList {
     List enemyBombs = new ArrayList<Bomb>();
     List myBombs = new ArrayList<Bomb>();
 
-    public BombsList(){}
+    public BombsList() {
+    }
 
-    public void resetList(){
+    public void resetList() {
         enemyBombs.clear();
         myBombs.clear();
     }
 
-    public void setBomb(Bomb bomb){
+    public void setBomb(Bomb bomb) {
         if (bomb.getOwner() == 1) myBombs.add(bomb);
         else enemyBombs.add(bomb);
     }
 
-    public List<Bomb> getEnemyBombs(){
+    public List<Bomb> getEnemyBombs() {
         return enemyBombs;
     }
 
-    public List<Bomb> getMyBombs(){
+    public List<Bomb> getMyBombs() {
         return myBombs;
     }
 
@@ -342,45 +387,48 @@ class Factory {
     private int production;
     private int delay;
 
-    public Factory(){};
+    public Factory() {
+    }
 
-    public void setId(int id){
+    ;
+
+    public void setId(int id) {
         this.id = id;
     }
 
-    public void setOwner(int owner){
+    public void setOwner(int owner) {
         this.owner = owner;
     }
 
-    public void setCyborgs(int cyborgs){
+    public void setCyborgs(int cyborgs) {
         this.cyborgs = cyborgs;
     }
 
-    public void setProduction(int production){
+    public void setProduction(int production) {
         this.production = production;
     }
 
-    public void setDelay(int delay){
+    public void setDelay(int delay) {
         this.delay = delay;
     }
 
-    public int getId(){
+    public int getId() {
         return id;
     }
 
-    public int getOwner(){
+    public int getOwner() {
         return owner;
     }
 
-    public int getCyborgs(){
+    public int getCyborgs() {
         return cyborgs;
     }
 
-    public int getProduction(){
+    public int getProduction() {
         return production;
     }
 
-    public int getDelay(){
+    public int getDelay() {
         return delay;
     }
 
@@ -393,45 +441,48 @@ class Troop {
     private int target;
     private int timeToObjective;
 
-    public Troop(){};
+    public Troop() {
+    }
 
-    public void setOwner(int owner){
+    ;
+
+    public void setOwner(int owner) {
         this.owner = owner;
     }
 
-    public void setCyborgs(int cyborgs){
+    public void setCyborgs(int cyborgs) {
         this.cyborgs = cyborgs;
     }
 
-    public void setOrigin(int origin){
+    public void setOrigin(int origin) {
         this.origin = origin;
     }
 
-    public void setTarget(int target){
+    public void setTarget(int target) {
         this.target = target;
     }
 
-    public void setTimeToObjective(int timeToObjective){
+    public void setTimeToObjective(int timeToObjective) {
         this.timeToObjective = timeToObjective;
     }
 
-    public int getOwner(){
+    public int getOwner() {
         return owner;
     }
 
-    public int getCyborgs(){
+    public int getCyborgs() {
         return cyborgs;
     }
 
-    public int getOrigin(){
+    public int getOrigin() {
         return origin;
     }
 
-    public int getTarget(){
+    public int getTarget() {
         return target;
     }
 
-    public int getTimeToObjective(){
+    public int getTimeToObjective() {
         return timeToObjective;
     }
 }
@@ -443,7 +494,10 @@ class Bomb {
     private int target;
     private int timeToObjective;
 
-    public Bomb() {};
+    public Bomb() {
+    }
+
+    ;
 
     public void setOwner(int owner) {
         this.owner = owner;
